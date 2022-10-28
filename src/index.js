b7cf8038a71e1cb989f87869d65728483df11ad5
Oliverio <oliverio.junior2@gmail.com>
2022-10-28 16:29:05 -0300

    Adicionar todos os métodos do express

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(req, res)=>{
-  return res.json({message: "hello world"})
+app.get('/courses',(req, res)=>{
+  return res.json(["Curso 1, Curso 2 , Curso 3"])
+})
+
+app.post('/courses',(req, res)=>{
+  return res.json(["Curso 1, Curso 2 , Curso 3, Curso 4"])
+})
+
+app.put('/courses/:id',(req, res)=>{
+  return res.json(["Curso 6, Curso 2 , Curso 3, Curso 4"])
+})
+
+app.patch('/course/:id',(req, res) =>{
+  return res.json(["Curso 6, Curso 7 , Curso 3, Curso 4"])
+})
+
+app.delete('/course/:id',(req, res) =>{
+  return res.json(["Curso 6, Curso 7 , Curso 4"])
 })
 
 app.listen(3333);
